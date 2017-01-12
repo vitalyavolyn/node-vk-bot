eebe38f10f0dfd3f6a9776cd7d03b10ba9ba0520
Eblonko <vitalya147@ya.ru>
2017-01-12 14:56:31 +0600

    use request instead of unirest (resolve #2)

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,4 +1,4 @@
-import unirest from 'unirest'
+import rq from 'request-promise-native'
 
 /**
  * Класс для обращения к API ВКонтакте
@@ -8,18 +8,18 @@ export default function API (token, version = 5.62, lang = 0) {
     params.lang = params.lang || lang
     params.v = params.v || version
     params.access_token = token
-    return new Promise((resolve, reject) => {
-      unirest.post('https://api.vk.com/method/' + method)
-      .field(params)
-      .end(res => {
-        if (!res.body) {
-          reject(res)
-        } else if (res.body.error) {
-          reject(res.body.error)
-        } else {
-          resolve(res.body.response)
-        }
-      })
+    return rq({
+      baseUrl: 'https://api.vk.com',
+      uri: '/method/' + method,
+      form: params,
+      method: 'POST',
+      json: true,
+      timeout: this.timeout
+    }).then(res => {
+      if (res.error) {
+        throw res.error
+      }
+      return res.response
     })
   }
 }
--- src/functions/poll.js
+++ src/functions/poll.js
@@ -1,16 +1,18 @@
-import unirest from 'unirest'
+import rq from 'request-promise-native'
 import sleep from './sleep'
 
 export default function poll (bot) {
-  return bot.api('messages.getLongPollServer').then(res => {
-    rq(`https://${res.server}?act=a_check&key=${res.key}` +
-      `&wait=25&mode=2&version=1&ts=${res.ts}`)
-  }).catch(err => console.log(err))
+  return bot.api('messages.getLongPollServer')
+    .then(res => {
+      request(`https://${res.server}?act=a_check&key=${res.key}` +
+        `&wait=25&mode=2&version=1&ts=${res.ts}`)
+    })
+    .catch(err => console.log(err))
 
-  function rq (url) {
-    unirest.get(url)
-      .end(res => {
-        res = JSON.parse(res.body)
+  function request (url) {
+    return rq(url, { json: true })
+      .catch(() => poll(bot))
+      .then(res => {
         if (!res || !res.ts || res.failed) return poll(bot) // перезапуск при ошибке
         url = url.replace(/ts=.*/, `ts=${res.ts}`) // ставим новое время
 
@@ -22,7 +24,7 @@ export default function poll (bot) {
         }
 
         if (bot._stop) return null
-        return sleep(500).then(() => rq(url))
+        return sleep(300).then(() => request(url))
       })
   }
 }
