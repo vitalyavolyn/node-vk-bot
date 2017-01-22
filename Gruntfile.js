a7ca69e3b613c729e6005b4f54472e2eebaed791
Eblonko <vitalya147@ya.ru>
2017-01-22 21:24:27 +0600

    tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       }
     },
     eslint: {
-      scripts: ['src/**/*.js']
+      scripts: ['src/**/*.js', 'test/**/*.js']
     },
     watch: {
       scripts: {
