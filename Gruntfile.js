c4fa50e0c93ec2f93f7ffd5c400ecc207c4feb94
Eblonko <vitalya147@ya.ru>
2017-02-03 22:08:20 +0600

    config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,36 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    clean: ['build']
+    clean: ['build'],
+    tslint: {
+      options: {
+        // can be a configuration object or a filepath to tslint.json
+        configuration: 'tslint.json',
+        // If set to true, tslint errors will be reported, but not fail the task
+        // If set to false, tslint errors will be reported, and the task will fail
+        force: false,
+        fix: false
+      },
+      files: {
+        src: [
+          'src/**/*.ts',
+          'test/**/*.ts'
+        ]
+      }
+    },
+    ts: {
+      default : {
+        src: ["**/*.ts", "!node_modules/**"],
+        outDir: 'build',
+        options: {
+          allowJs: true
+        }
+      }
+    }
   })
 
   grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-tslint')
+  grunt.loadNpmTasks('grunt-ts')
 
-  grunt.registerTask('default', ['clean'])
+  grunt.registerTask('default', ['clean', 'ts'])
 }
