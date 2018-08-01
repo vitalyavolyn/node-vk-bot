c73fd079a2b6d8574810fc43ff4687835cb504a1
Vitaly Volynsky <me@vitalyavolyn.me>
2018-08-01 22:39:34 +0600

    Update MessageSendParams
    Add keyboard, payload

--- src/interfaces/MessageSendParams.ts
+++ src/interfaces/MessageSendParams.ts
@@ -4,13 +4,12 @@ export interface MessageSendParams {
   random_id?: number,
   domain?: string,
   chat_id?: string,
-  user_ids?: string, // list of comma-separated numbers
+  user_ids?: string, // list of comma-separated numbers, max 100
   lat?: number,
   long?: number,
   attachment?: string,
   forward_messages?: string,
   sticker_id?: number,
-
-  // deprecated
-  guid?: number
+  keyboard?: string,
+  payload?: any
 }
